0746ab74bf24dae4d4561d5fc42bbd1dea6407af
emmafelix <emma.felix@iseg.fr>
2020-06-29 10:46:03 +0200

    Day5-Exercice2

---
+++ EX_02/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+                <a class="encours" href="vitrine-acceuil.php">Acceuil</a>
+                <a href="vitrine-contacts.php">Contact</a>
+                <a href="vitrine-programme.php">Programme</a>
+            </nav>
---
+++ EX_02/vitrine-contacts.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" href="../style/vitrine.css">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <header>
+    <?php
+       include ("header.php");
+       ?>
+    </header>
+    <div class="contenu">
+        <h2>
+            Contact
+        </h2>
+        <form>
+            <div class="champs">
+                
+                <input type="text" placeholder="Votre nom">
+                <br>
+                
+                <input type="email" placeholder="Votre email">
+
+                
+                <input class="messagepart" placeholder="Comment améliorer mon site ?">
+                <div class="bouton">
+                    <button type="submit">Envoyer</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <footer>
+    <?php
+        include ("footer.php");
+        ?>
+    </footer>
+    </div>
+</body>
+
+</html>
